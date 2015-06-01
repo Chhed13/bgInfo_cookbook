926d7cacffe2aa98c8b035996854bdd3015c7ba3
andrey <arotchev@wildapricot.com>
2015-06-01 11:08:14 +0300

    updated template

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email "etrufanov@wildapricot.com"
 license          "Apache 2.0"
 description      "Installs and configures Microsoft Technet Sysinternals BgInfo"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
+version          "1.0.1"
 supports         "windows"
 depends          "windows", ">= 1.3.6"
